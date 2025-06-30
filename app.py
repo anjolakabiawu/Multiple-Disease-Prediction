5ef4d07a30490dcb31c098fcf5a19eabef7d911c
Anjola <anjolakabiawu@yahoo.com>
2025-06-30 15:45:41 +0100

    Fixed the model and library versions problem

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 import joblib
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+# Loading the saved models
+diabetes_model = joblib.load('C:/Users/DELL/Desktop/ML Projects/Multiple Disease App/models/diabetes_model.sav')
+heart_model = joblib.load('models/heart_disease_model.sav')
+parkinson_model = joblib.load('models/parkinson_disease_model.sav')
